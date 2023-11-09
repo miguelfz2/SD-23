8ff27e618b1e1674a848366049d687a6c40f2fca
mfz2 <mfz2@gcloud.ua.es>
2023-11-09 10:42:58 +0100

    scripts probados

--- pc1.bat
+++ pc1.bat
@@ -1,7 +1,6 @@
 @echo off
-cd weather
-python3 AD_Weather.py %1
-cd C:\bin\windows\
-call zookeeper-server-start.bat .\config\zookeeper.properties
-call kafka-server-start.bat .\config\server.properties
+start cmd /k "python C:\Users\EPS\Desktop\SD-23\weather\AD_Weather.py %1"
+start cmd /k "call C:\kafka\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties"
+timeout /t 2 /nobreak >nul 2>&1
+start cmd /k "call C:\kafka\bin\windows\kafka-server-start.bat .\config\server.properties"
 
--- pc2.bat
+++ pc2.bat
@@ -1,12 +1,9 @@
 @echo off
 if "%~6"=="" (
-	echo Uso : %0 <Puerto de escucha> <MAX_CONEXIONES> <IP AD_Weather/kafka> <Puerto kafka> <Puerto AD_Weather>
+	echo "Uso : %0 <Puerto de escucha> <MAX_CONEXIONES> <IP AD_Weather/kafka> <Puerto kafka> <Puerto AD_Weather>"
 )
-cd registry
 rem <Puerto registro>
-python AD_registry.py %1
-cd ..
-cd engine
+start cmd /k "python C:\Users\EPS\Desktop\SD-23\registry\AD_registry.py %1"
 rem <Puerto de escucha> <MAX_CONEXIONES> <IP AD_Weather/kafka> <Puerto kafka> <Puerto AD_Weather>
-python AD_engine %2 %3 %4 %5 %6 
+start cmd /k "python C:\Users\EPS\Desktop\SD-23\engine\AD_engine.py %2 %3 %4 %5 %6
 
